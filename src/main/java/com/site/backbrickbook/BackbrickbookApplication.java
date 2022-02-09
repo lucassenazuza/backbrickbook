86b7a04ff83a6d0f5791194dfc700aca3f9bd79f
lucassenazuza <lucassenazuza@gmail.com>
2022-02-08 23:23:17 -0300

    first

---
+++ src/main/java/com/site/backbrickbook/BackbrickbookApplication.java
@@ -0,0 +1,13 @@
+package com.site.backbrickbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackbrickbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackbrickbookApplication.class, args);
+	}
+
+}
