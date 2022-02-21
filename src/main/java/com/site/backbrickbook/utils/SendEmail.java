aa5e752b53f62fa08576f8e97cb19cf81d44d7c3
Lucas <uWA%VblDP1>
2022-02-21 18:04:40 -0300

    mod

--- src/main/java/com/site/backbrickbook/utils/SendEmail.java
+++ src/main/java/com/site/backbrickbook/utils/SendEmail.java
@@ -1,15 +1,14 @@
 package com.site.backbrickbook.utils;
 
-package com.mkyong;
 
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+
 import java.util.Properties;
 
 public class SendEmail {
 
     public static void main(String[] args) {
+
+
 
         final String username = "username@gmail.com";
         final String password = "password";
---
+++ src/main/java/com/site/backbrickbook/utils/teste.java
@@ -0,0 +1,26 @@
+package com.site.backbrickbook.utils;
+
+
+import java.io.FileReader;
+
+public class teste {
+    public static void main(String[] args) {
+//        JSONParser parser = new JSONParser();
+//        try {
+//            Object obj = parser.parse(new FileReader("/Users/User/Desktop/course.json"));
+//            JSONObject jsonObject = (JSONObject)obj;
+//            String name = (String)jsonObject.get("Name");
+//            String course = (String)jsonObject.get("Course");
+//            JSONArray subjects = (JSONArray)jsonObject.get("Subjects");
+//            System.out.println("Name: " + name);
+//            System.out.println("Course: " + course);
+//            System.out.println("Subjects:");
+//            Iterator iterator = subjects.iterator();
+//            while (iterator.hasNext()) {
+//                System.out.println(iterator.next());
+//            }
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+}
